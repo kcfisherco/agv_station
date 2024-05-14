7bb48f2d95b58ef93e00adb7396ef37eaaef4e59
kidd <kidd@amr.fisherco.com>
2024-05-14 11:52:48 -0400

    Move class vars to global
    Changed gpio variables so that they were global variables, this way they can properly be cleaned up

--- src/amr_v4_station/amr_v4_station/station.py
+++ src/amr_v4_station/amr_v4_station/station.py
@@ -7,8 +7,9 @@ from rclpy.executors import MultiThreadedExecutor
 from threading import Thread
 
 class Sensor(Node):
-    def __init__(self):
-        super().__init__("sensor_status")
+    pass
+    #     def __init__(self):
+    #     super().__init__("sensor_status")
     #     self.publisher = self.create_publisher(bool, "sensor_status", 10)
     #     self.timer = self.create_timer(1.0, self.callback)
 
@@ -18,15 +19,7 @@ class Sensor(Node):
 class Relay(Node):
     def __init__(self):
         super().__init__("relay_status")
-        #Temp
-        self.pin_output = 7
-        self.pin_input = [29, 33, 40]
-        #Temp
-
-        self.input_29 = GPIO.setup(29, GPIO.OUT, initial=GPIO.LOW)
-        self.input_33 = GPIO.setup(33, GPIO.OUT, initial=GPIO.LOW)
-        self.input_40 = GPIO.setup(40, GPIO.OUT, initial=GPIO.LOW)
-        #self.relay_subscriber = self.create_subscription(bool, "relay_status", self.display_relay_status(), 10)
+        # self.relay_subscriber = self.create_subscription(bool, "relay_status", self.display_relay_status(), 10)
 
     def display_relay_status(self, msg):
         self.logging().info(msg.data)
@@ -44,32 +37,34 @@ class Relay(Node):
             prev_value = value
 
     def simulator(self):
-        GPIO.output(self.pin_input[0], GPIO.HIGH)
-        print("Outputting state: {} to pin: {}".format(GPIO.input(self.pin_input[0]), self.pin_input[0]))
-        print("sleep 1000 seconds :D")
-        time.sleep(1000)
-        #print("Input Pin 29: ", GPIO.input(29))
+        GPIO.output(pin_output[0], GPIO.HIGH)
+        print("Outputting state: {} to pin: {}".format(GPIO.input(pin_output[0]), pin_output[0]))
+        # print("Input Pin 29: ", GPIO.input(29))
         # GPIO.setup(29, GPIO.IN)
-        # GPIO.setup(self.pin_input[0], GPIO.IN)
-        # GPIO.wait_for_edge(self.pin_input[0], GPIO.RISING)
+        # GPIO.setup(pin_output[0], GPIO.IN)
+        # GPIO.wait_for_edge(pin_output[0], GPIO.RISING)
         # flag_relay_receiver()
         # GPIO.setup(29, GPIO.OUT)
-        # GPIO.output(self.pin_input[0], GPIO.LOW)
+        # GPIO.output(pin_output[0], GPIO.LOW)
 
 """Global Settings"""
+pin_input = 7
+pin_output = [29, 33, 40]
 
 def main():
 
     rclpy.init(args=None)
-    GPIO.setwarnings("ignore")
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin_input, GPIO.IN)
+    GPIO.setup(pin_output, GPIO.OUT, initial=GPIO.LOW)
 
     try:
 
-        sensor = Sensor()
+        # sensor = Sensor()
         relay = Relay()
         executor = MultiThreadedExecutor()
-        executor.add_node(sensor)
+        # executor.add_node(sensor)
         executor.add_node(relay)
         logging.info("Initializing Station")
         executor_try = Thread(target=executor.spin,daemon=True)
@@ -80,14 +75,12 @@ def main():
             sys.exit()
 
     except (KeyboardInterrupt,SystemError,SystemExit,rclpy.exceptions.ROSInterruptException):
-
-        
         exit()
 
     finally:
         GPIO.cleanup()
-        sensor.destroy_node()
-        relay.destoy_node()
+        # sensor.destroy_node()
+        # relay.destoy_node()
         rclpy.shutdown()
         print("shutting down :)")
 
